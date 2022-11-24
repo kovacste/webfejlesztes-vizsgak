0b99e08b36e820b14ed798e49b1cc30f5baf04db
kovacste <kovacst.elod@gmail.com>
2022-11-24 19:58:45 +0100

    refactor

--- 2019-20-2_02/index.js
+++ 2019-20-2_02/index.js
@@ -1,31 +1,24 @@
-let resultElement = document.getElementById('result');
-let radiusInput = document.getElementById('radius');
-let lengthInput = document.getElementById('length');
-let levelInput = document.getElementById('level');
-let levelLabel = document.getElementById('levelLabel');
+window.addEventListener('load', () => {
 
-function updateResult(result) {
-    resultElement.innerText = result;
-}
+    let resultElement = document.getElementById('result');
+    let radiusInput = document.getElementById('radius');
+    let lengthInput = document.getElementById('length');
+    let levelInput = document.getElementById('level');
+    let levelLabel = document.getElementById('levelLabel');
 
-function updateLevel(level) {
-    levelLabel.innerText = level;
-}
+    function calculateVolume(r, l, h) {
+        return r * r * l * (Math.acos((r - h) / r) - (r - h) * Math.sqrt(2 * r * h - h * h) / (r * r));
+    }
 
-function calculateVolume(r, l, h) {
-    return r * r * l * (Math.acos((r - h) / r) - (r - h) * Math.sqrt(2 * r * h - h * h) / (r * r));
-}
+    function onInputChanged() {
+        let radius = radiusInput.valueAsNumber;
+        let length = lengthInput.valueAsNumber;
+        let level = levelInput.valueAsNumber;
+        levelLabel.innerText = level;
+        levelInput.max = radius * 2;
+        resultElement.innerText = calculateVolume(radius, length, level);
+    }
 
-function onInputChanged() {
-    let radius = parseFloat(radiusInput.value);
-    let length = parseFloat(lengthInput.value);
-    let level = parseFloat(levelInput.value);
-    let volume = calculateVolume(radius, length, level);
-    updateResult(volume);
-    updateLevel(level);
-}
-
-window.addEventListener('load', function(){
     radiusInput.addEventListener('input', onInputChanged);
     lengthInput.addEventListener('input', onInputChanged);
     levelInput.addEventListener('input', onInputChanged);
